b288067265b7c471ae41ffa692ba6442573fdd28
藍月暗い <Kurai32761@gmail.com>
2025-01-05 17:24:01 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { SlashCommandBuilder } = require('discord.js');
+const config = require("../config.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('say')
