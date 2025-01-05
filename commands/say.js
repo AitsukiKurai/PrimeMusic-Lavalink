8f3db0e28af50189f7bf6e041c924c4219ecb6f9
藍月暗い <Kurai32761@gmail.com>
2025-01-05 16:48:08 +0700

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+data: new SlashCommandBuilder()
+.setName('say')
+.setDescription('Make the bot repeat a message')
+.addStringOption(option =>
+option.setName('message')
+.setDescription('The message to repeat')
+.setRequired(true)
+),
+async execute(interaction, client) {
+const message = interaction.options.getString('message');
+await interaction.reply(message);
+},
+};
