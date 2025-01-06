7b0ed34d8c4d66271ca99c6d1c48327eaa28b65e
藍月暗い <Kurai32761@gmail.com>
2025-01-06 18:15:49 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,7 +29,7 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
+      const supportServerLink = "https://discord.gg/3KRSK7eF3H";
       const githubLink = "https://github.com/GlaceYT";
       const replitLink = "https://replit.com/@GlaceYT";
       const youtubeLink = "https://www.youtube.com/@GlaceYT";
