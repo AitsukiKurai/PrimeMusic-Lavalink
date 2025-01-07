7528ec4ee22754703ce6d77cce87d7b85eab43ed
藍月暗い <Kurai32761@gmail.com>
2025-01-08 06:37:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -36,10 +36,10 @@ module.exports = {
 
 nodes: [
     {
-        name: "UWU",
-        host: "lava.inzeworld.com",
-        port: 3128,
-        password: "saher.inzeworld.com",
+        name: "Arise",
+        host: "lavav4.prplmoe.me",
+        port: 1118,
+        password: "prplmoe.me",
         reconnectTimeout: 5000,
         reconnectTries: Infinity,
         secure: false
