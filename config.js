a676e36c552ac67609b571a8a605bdea41b74a0a
藍月暗い <Kurai32761@gmail.com>
2025-03-01 18:54:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ module.exports = {
   mongodbUri : "",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  embedColor: "#1db954", 
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
