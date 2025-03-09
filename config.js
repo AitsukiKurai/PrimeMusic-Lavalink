310afa4e5e3eb529b85451dbbd9985037684cf2e
藍月暗い <Kurai32761@gmail.com>
2025-03-09 14:04:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "tess" ,
+  activityName: "聲の形" ,
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
   errorLog: "", 
