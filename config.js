eae9ada23d25e240bf301a9ce8be9a8148f48e02
藍月暗い <Kurai32761@gmail.com>
2025-06-06 10:11:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
 {
       name: ":D",
-      password: "wwweasycodero",
-      host: "lavalink-v2.pericsq.ro",
-      port:  6677,
+      password: "anakin",
+      host: "us.apsara.lol",
+      port:  25575,
       secure: false
 }
   ]
