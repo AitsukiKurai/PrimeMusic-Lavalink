cbfdcffed9a4be16eeaefe4d13a2b75805a257eb
藍月暗い <Kurai32761@gmail.com>
2025-07-14 14:51:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
 {
-      name: "GlaceYT",
-      password: "anakin",
-      host: "us.apsara.lol",
-      port:  25575,
+      name: "XD",
+      password: "youshallnotpass",
+      host: "gh46.glacierhosting.org",
+      port:  25583,
       secure: false
 }
   ]
