722c997655ae31b9a8e3712479d848fb9badd380
藍月暗い <Kurai32761@gmail.com>
2025-08-18 13:10:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "聲の形" ,
+  activityName: "Lời trăn chối của những kẻ sắp bị hốc" ,
   ActivityType: "Listening" ,
   SupportServer: "https://discord.gg/xQF9f9yUEM",
   embedTimeout: 5, 
