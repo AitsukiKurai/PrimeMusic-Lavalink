3682026803223ca9d51e37c4314aed373585ae38
藍月暗い <Kurai32761@gmail.com>
2025-09-27 13:38:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ module.exports = {
   nodes: [
 {
       name: "XD",
-      password: "youshallnotpass",
-      host: "lavalink.jirayu.net",
-      port:  13592,
+      password: "glace",
+      host: "5.39.63.207",
+      port:  8262,
       secure: false
 }
   ]
